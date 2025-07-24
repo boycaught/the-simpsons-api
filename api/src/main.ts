a647703b2b68146d548b84352c59577724fe582e
Facug03 <facundogonzales1550@gmail.com>
2025-07-24 04:06:17 -0300

    fix: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,7 +9,9 @@ import { AppModule } from '@/src/app.module'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
-  app.enableCors()
+  app.enableCors({
+    origin: '*'
+  })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   app.setGlobalPrefix('api')
   await app.register(helmet, {
